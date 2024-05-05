3d4f642e8ee198bc1f298823f73406d30add4a0c
Tuyen Ha <tuyen3962@gmail.com>
2024-05-05 17:13:12 +0700

    update flutter sdk

--- lib/vnpay.dart
+++ lib/vnpay.dart
@@ -5,16 +5,16 @@ import 'package:flutter/services.dart';
 class VnPay {
   static const MethodChannel _channel = const MethodChannel('flutter.io/vnpay');
   static Future<String> payment({
-    String isSandbox,
-    String scheme,
-    String appBackAlert,
-    String url,
-    String title,
-    String backIcon,
-    String beginColor,
-    String endColor,
-    String titleColor,
-    String tmn_code,
+    required String url,
+    String? isSandbox,
+    String? scheme,
+    String? appBackAlert,
+    String? title,
+    String? backIcon,
+    String? beginColor,
+    String? endColor,
+    String? titleColor,
+    String? tmn_code,
   }) async {
     Map<String, String> dict = {
       "isSandbox": isSandbox ?? "false",
